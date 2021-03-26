50989d908157b6cdf762b7ee8ccf8958071d1fff
GuillePujals <guille_pujals@yahoo.com.ar>
2021-03-25 21:31:55 -0300

    Guille - first commit estructura del proyecto

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/login/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/productCart/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'));
+});
+
+app.get('/productDetail/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
+
+app.get('/register/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.listen(port, () => {
+    console.log('Página de alquiler de casas '+ port);
+});
